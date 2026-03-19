668e948d261722f5910260ba9ec9effec7c7e7d0
hiteshpurohit707-hue <hiteshpurohit707@gmail.com>
2026-03-19 16:01:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+// lock scroll
+document.body.style.overflow = "hidden";
+
+// loader
+let progress = 0;
+const loader = document.getElementById("loader");
+const progressSpan = document.getElementById("progress");
+const circle = document.getElementById("progress-bar");
+
+const radius = 90;
+const circumference = 2 * Math.PI * radius;
+
+circle.style.strokeDasharray = circumference;
+
+function setProgress(p){
+circle.style.strokeDashoffset = circumference - (p/100)*circumference;
+}
+
+const interval = setInterval(()=>{
+progress++;
+progressSpan.textContent = progress;
+setProgress(progress);
+
+if(progress >= 100){
+clearInterval(interval);
+loader.style.opacity = "0";
+
+setTimeout(()=>{
+loader.style.display = "none";
+document.body.style.overflow = "auto";
+},500);
+}
+
+},20);
+
+
+// scroll animation
+function reveal(){
+document.querySelectorAll(".reveal").forEach(el=>{
+if(el.getBoundingClientRect().top < window.innerHeight - 100){
+el.classList.add("active");
+}
+});
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
+
+
+// mobile menu
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", ()=>{
+nav.classList.toggle("active");
+});
